1f29a9dfb86092fc4e2004677bb8f1ce38e41ed2
Eka Fridayanti <ekafridayanti25@gmail.com>
2021-11-05 15:58:28 +0800

    eka 2 commit

---
+++ database/migrations/2021_11_05_074100_create_transaksi_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransaksiTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transaksi', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('id_mahasiswa')->references('id')->on('mahasiswa');
+            $table->foreignId('id_buku')->references('id')->on('buku');
+            $table->datetime('tanggal_pinjam');
+            $table->datetime('tanggal_kembali')->nullable();
+            $table->boolean('status_pinjam');
+            $table->decimal('total_biaya', 12, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transaksi');
+    }
+}
