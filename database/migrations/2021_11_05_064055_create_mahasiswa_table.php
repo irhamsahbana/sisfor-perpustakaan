c95ff228e8bb223eb79ab8d045e32ba3a33ea45e
irhamsahbana <irhamsahbana@gmail.com>
2021-11-05 15:08:24 +0800

    migrasi tabel mahasiswa

---
+++ database/migrations/2021_11_05_064055_create_mahasiswa_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMahasiswaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mahasiswa', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama');
+            $table->string('nim', 15);
+            $table->string('email');
+            $table->string('no_telp', 15);
+            $table->string('prodi');
+            $table->string('jurusan');
+            $table->string('fakultas');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mahasiswa');
+    }
+}
